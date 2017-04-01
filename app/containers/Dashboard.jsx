f842fb17b3d9b369db96e604ffe66b96d6452e10
Rike Rohde <Rike Rohde>
2017-04-01 11:36:08 +0200

    [TRY] process.cwd resolve on content renderer

--- app/containers/Dashboard.jsx
+++ app/containers/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import Markdown from 'react-remarkable';
+const path = require('path');
 import styles from '../css/components/dashboard';
 
 const cx = classNames.bind(styles);
 
-const content = require('../content/dashboard.md');
+path.resolve(process.cwd(), 'app/content/dashboard.md');
 
 const Dashboard = () => (
   <div className={cx('container')}>
